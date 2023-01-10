bc245d0ba37f4215bda0ab001f366f3969ece704
Den-Chum <denis.chumachenko.dcb@gmail.com>
2023-01-10 02:28:28 +0200

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-const swiper = new Swiper('.swiper', {
+const hotelSwiper = new Swiper(".hotel-swiper", {
   loop: true,
 
   navigation: {
-    nextEl: '.swiper-button--next',
-    prevEl: '.swiper-button--prev',
+    nextEl: ".hotel-swiper__button--next",
+    prevEl: ".hotel-swiper__button--prev",
+  },
+});
+
+const reviewsSwiper = new Swiper(".reviews-swiper", {
+  loop: true,
+
+  navigation: {
+    nextEl: ".reviews-swiper__button--next",
+    prevEl: ".reviews-swiper__button--prev",
   },
 });
